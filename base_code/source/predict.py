06f36e3b56bc4c7048baed8720e244c31e1c5298
fares-23 <adam.akil@etu.sorbonne-universite.fr>
2025-05-03 21:56:33 +0200

    test sur predict

--- base_code/source/predict.py
+++ base_code/source/predict.py
@@ -7,8 +7,8 @@ from torchvision import transforms
 from model import HybridModel
 from torchvision.models import ResNet50_Weights
 
-DATA_DIR_CHRISTELLE = r"C:\Users\Christelle\Documents\CHALLENGE\images"
-
+#DATA_DIR_CHRISTELLE = r"C:\Users\Christelle\Documents\CHALLENGE\images"
+#DATA_DIR_ADAM = r"/Volumes/TOSHIBA/CHALLENGE/images"
 class Predictor:
     def __init__(self):
         weights = ResNet50_Weights.DEFAULT
@@ -55,6 +55,25 @@ class Predictor:
 
 if __name__ == "__main__":
     predictor = Predictor()
-    predictor.predict(DATA_DIR_CHRISTELLE)
+    #predictor.predict(DATA_DIR_CHRISTELLE)
+    predictor.predict(DATA_DIR_ADAM)
 
     print("Prediction completed and saved to evaluation/test/output/detected-inflammatory-cells.json")
+
+J'utilise ce code et j'ai cette erreur : 
+HybridModel loaded successfully.
+TIFFFillTile: 0: Invalid tile byte count, tile 0.
+Traceback (most recent call last):
+  File "/Users/adamakil/Desktop/Traitement des Images/challenge/Challenge_TI/base_code/source/predict.py", line 59, in <module>
+    predictor.predict(DATA_DIR_ADAM)
+  File "/Users/adamakil/Desktop/Traitement des Images/challenge/Challenge_TI/base_code/source/predict.py", line 37, in predict
+    image = Image.open(img_path).convert("RGB")
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/PIL/Image.py", line 995, in convert
+    self.load()
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/PIL/TiffImagePlugin.py", line 1237, in load
+    return self._load_libtiff()
+           ^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/PIL/TiffImagePlugin.py", line 1342, in _load_libtiff
+    raise OSError(err)
+OSError: -2
